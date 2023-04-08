555e02637139e4a34bd0e2680c809038d4f561d1
ricky <yricky509@gmail.com>
2023-04-08 22:21:42 +0800

    finish: string enums modules
    Signed-off-by: ricky <yricky509@gmail.com>

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -1,21 +1,25 @@
 // strings3.rs
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
+use std::fmt::format;
 
 fn trim_me(input: &str) -> String {
-    // TODO: Remove whitespace from both ends of a string!
-    ???
+    // Remove whitespace from both ends of a string!
+    // input.trim().to_string()
+    format!("{}", input.trim())
 }
 
 fn compose_me(input: &str) -> String {
-    // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    // Add " world!" to the string! There's multiple ways to do this!
+    // format!("{} world!", input)
+    input.to_string() + " world!"
 }
 
 fn replace_me(input: &str) -> String {
-    // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    // Replace "cars" in the string with "balloons"!
+    // input.replace("cars", "balloons")
+    format!("{}", input.replace("cars", "balloons"))
 }
 
 #[cfg(test)]
